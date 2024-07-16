bbb889a418adb068d98f696ba06744b934032fc4
Emiliano Perez <emi.perez997@gmail.com>
2024-07-16 17:54:19 -0300

    [ADD] Challenge 1

---
+++ src/app/modules/layout/layout.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { TableComponent } from '../../components/students/table/table.component';
+import { TitleDirective } from '../../common/directives/title.directive';
+
+@NgModule({
+  declarations: [LayoutComponent],
+  imports: [
+    CommonModule,
+    ToolbarComponent,
+    SidenavComponent,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    TableComponent,
+    TitleDirective,
+  ],
+  exports: [LayoutComponent],
+})
+export class LayoutModule {}

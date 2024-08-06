486fc97a0c5a572de8fde4bc5520208625a49506
Emiliano Perez <emi.perez997@gmail.com>
2024-08-06 10:52:42 -0300

    [ADD] Auth Service Testing

--- src/app/features/auth/login/login.component.spec.ts
+++ src/app/features/auth/login/login.component.spec.ts
@@ -1,18 +1,62 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { MockProvider } from 'ng-mocks';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { ThemeService } from '../../../core/services/theme/theme.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { AngularToastifyModule } from 'angular-toastify';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
-// describe('LoginComponent', () => {
-//   let component: LoginComponent;
-//   let fixture: ComponentFixture<LoginComponent>;
-//
-//   // beforeEach(async () => {
-//   //   await TestBed.configureTestingModule({
-//   //     imports: [LoginComponent],
-//   //   }).compileComponents();
-//   //
-//   //   fixture = TestBed.createComponent(LoginComponent);
-//   //   component = fixture.componentInstance;
-//   //   fixture.detectChanges();
-//   // });
-// });
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let themeService: ThemeService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatCardModule,
+        MatIconModule,
+        MatButtonModule,
+        AngularToastifyModule,
+      ],
+      providers: [
+        AuthService,
+        ThemeService,
+        provideAnimationsAsync(),
+        provideHttpClient(),
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    themeService = TestBed.inject(ThemeService);
+    fixture.detectChanges();
+  });
+
+  it('Should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should toggle theme', () => {
+    spyOn(themeService, 'setTheme');
+
+    component.toggleTheme();
+
+    expect(themeService.setTheme).toHaveBeenCalled();
+  });
+});
